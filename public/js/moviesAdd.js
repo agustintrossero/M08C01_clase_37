9a633764844a9e710870812996927f72b3d88883
agustintrossero <agustintrossero@gmail.com>
2022-04-28 19:11:15 +0200

    ejercicio completo sin bonus track

---
+++ public/js/moviesAdd.js
@@ -0,0 +1,19 @@
+
+let h1 = document.querySelector("h1")
+let section = document.querySelector("section") 
+let article =  document.querySelector("article")
+
+h1.innerText = "AGREGAR PELÍCULAS"
+h1.classList.add("titulo")
+
+article.classList.add("fondoTransparente")
+
+section.classList.add("fondoCRUD")
+
+/*
+
+●        Agregar a la etiqueta <h1> el mensaje: “AGREGAR PELÍCULAS”.  
+●        Agregar a la etiqueta <h1> la clase: “titulo”. 
+●        Agregar al artículo la clase: “fondoTransparente”. 
+●        Agregar a la sección la clase: “fondoCRUD”. 
+*/
--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -14,13 +14,3 @@ h1.style.backgroundColor = "Teal"
 h1.style.padding  = "20px"
 
 
-/*
-Crear un archivo JavaScript: /public/js//moviesAdd.js, y vincularlo con el archivo; 
-/views//moviesAdd.ejs. 
-●        Desde el archivo /public/js/moviesAdd.js, capturar los siguientes elementos: <h1>, 
-<section> y <article>, ubicados en el archivo: /views//moviesAdd.ejs.  
-●        Agregar a la etiqueta <h1> el mensaje: “AGREGAR PELÍCULAS”.  
-●        Agregar a la etiqueta <h1> la clase: “titulo”. 
-●        Agregar al artículo la clase: “fondoTransparente”. 
-●        Agregar a la sección la clase: “fondoCRUD”. 
-*/
