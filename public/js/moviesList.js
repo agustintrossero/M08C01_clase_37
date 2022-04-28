5ae49b2875181a33878b0959dc96ad200da0e2a5
agustintrossero <agustintrossero@gmail.com>
2022-04-28 18:55:39 +0200

    micro desafio 2 completado

--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -2,19 +2,25 @@
 let body = document.querySelector("body")
 let h1 = document.querySelector("h1")
 
+let modoOscuro = prompt("¿Desea modo oscuro?")
+if (modoOscuro == "SI"){
+    body.style.backgroundColor = "#7f7f7f"
+    body.classList.add("fondoMoviesList")
+}
 
-
-console.log("HOLA movies")
-
-
-
-
+h1.innerText = "LISTADO DE PELÍCULAS"
+h1.style.color = "White"
+h1.style.backgroundColor = "Teal"
+h1.style.padding  = "20px"
 
 
 /*
-
-Haciendo uso del prompt, preguntar al usuario: “¿Desea modo oscuro?”. Si la 
-respuesta es afirmativa, agregar a la etiqueta <body> un color de fondo: #7f7f7f​, y 
-la clase: fondoMoviesList. 
-
+Crear un archivo JavaScript: /public/js//moviesAdd.js, y vincularlo con el archivo; 
+/views//moviesAdd.ejs. 
+●        Desde el archivo /public/js/moviesAdd.js, capturar los siguientes elementos: <h1>, 
+<section> y <article>, ubicados en el archivo: /views//moviesAdd.ejs.  
+●        Agregar a la etiqueta <h1> el mensaje: “AGREGAR PELÍCULAS”.  
+●        Agregar a la etiqueta <h1> la clase: “titulo”. 
+●        Agregar al artículo la clase: “fondoTransparente”. 
+●        Agregar a la sección la clase: “fondoCRUD”. 
 */
